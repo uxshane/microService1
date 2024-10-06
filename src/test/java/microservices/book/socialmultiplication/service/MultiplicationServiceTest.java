4bd9f702806a811cf2f9e095a38e7aa05e1651e0
Min Kyu Kim <uxchampaign@gmail.com>
2024-10-06 14:50:36 +0900

    distribution of workloads for creating classes that can store two numbers, randomly generate two numbers

--- src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceTest.java
+++ src/test/java/microservices/book/socialmultiplication/service/MultiplicationServiceTest.java
@@ -2,16 +2,16 @@ package microservices.book.socialmultiplication.service;
 
 import microservices.book.socialmultiplication.domain.Multiplication;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MultiplicationServiceTest {
 
